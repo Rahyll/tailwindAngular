0af867538b9c62329057f8cb8524661b7dd979cf
rahyll <rahil_sami@proton.me>
2025-03-26 16:42:12 +0530

    `Update ErrorwrapperComponent to use contentChild and input decorators correctly`

--- src/app/shared/components/errorwrapper/errorwrapper.component.ts
+++ src/app/shared/components/errorwrapper/errorwrapper.component.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChild, contentChild, input } from '@angular/core';
+import { Component, contentChild, input } from '@angular/core';
 import { Form, FormControl, NgControl } from '@angular/forms';
 import { MessageModule } from 'primeng/message';
 
@@ -9,34 +9,32 @@ import { MessageModule } from 'primeng/message';
   styleUrl: './errorwrapper.component.scss',
 })
 export class ErrorwrapperComponent {
-  @ContentChild(NgControl) control1!: FormControl;
-  readonly control = input<FormControl | null>(null);
+  readonly control = contentChild.required(NgControl);
   readonly label = input<string>('');
   readonly id = input<string>('');
   readonly ErrorLabel = input<string>('');
 
   getErrorMessage(): string {
-    if (!this.control1) return '';
+    const control = this.control();
+    if (!control) return '';
 
     const ErrorLabel = this.ErrorLabel();
     const errorMessages: { [key: string]: string } = {
-      required: `${
-        ErrorLabel ? ErrorLabel : this.label()
-      } is required.`,
+      required: `${ErrorLabel ? ErrorLabel : this.label()} is required.`,
       underage: `Underage`,
       minlength: `Minimum length is ${
-        this.control1.getError('minlength')?.requiredLength
+        control.getError('minlength')?.requiredLength
       } characters.`,
       maxlength: `Maximum length is ${
-        this.control1.getError('maxlength')?.requiredLength
+        control.getError('maxlength')?.requiredLength
       } characters.`,
       email: 'Enter a valid email address.',
     };
 
-    // Check if the control1 is touched and dirty
-    if (this.control1.dirty) {
-      for (const errorKey in this.control1.errors) {
-        if (this.control1.hasError(errorKey)) {
+    // Check if the control is touched and dirty
+    if (control.dirty) {
+      for (const errorKey in control.errors) {
+        if (control.hasError(errorKey)) {
           return errorMessages[errorKey] || 'Invalid input.';
         }
       }
